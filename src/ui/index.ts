de5d9458e48d5d6f1c76ba5375d6640c96463835
yoonbeeseo <yoon.tec.info@gmail.com>
2025-03-12 13:42:57 +0900

    reset init

--- src/ui/index.ts
+++
@@ -1,2 +0,0 @@
-export * as TextInput from "./TextInput";
-export * as Container from "./Container";
--- src/ui/useMySelect.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const useMySelect = () => {
-  return <div>useMySelect</div>;
-};
-
-export default useMySelect;
